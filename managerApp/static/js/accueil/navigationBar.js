91a0b3cc680d41671a70116dc7ddfa5b7dd150e2
Coco836 <coletteanselme@live.fr>
2024-03-18 22:47:41 +0900

    remove unused script

--- managerApp/static/js/accueil/navigationBar.js
+++ managerApp/static/js/accueil/navigationBar.js
@@ -3,89 +3,77 @@ export function navigateToSection(sectionId) {
 }
 
 document.addEventListener("scroll", function () {
-  // Récupérez la position actuelle de la page avec un ajustement pour une meilleure détection
-  const scrollPosition = $(window).scrollTop() + 150;
+  const scrollPosition = window.scrollY + 150;
+  const selectOptions = document.querySelectorAll("#mobileNavDropdown option");
 
-  // Votre sélecteur est différent puisque vous travaillez avec un <select>, donc récupérez les options
-  const selectOptions = $("#mobileNavDropdown option");
+  selectOptions.forEach(function (option) {
+    const targetId = option.value;
+    const targetElement = document.querySelector(targetId);
 
-  selectOptions.each(function () {
-    const targetId = $(this).val(); // L'attribut "value" des options contient l'ID de la section
-    const targetElement = $(targetId);
-
-    if (targetElement.length) {
-      // Vérifiez si la section correspondante est visible à l'écran
+    if (targetElement) {
+      const targetPosition =
+        targetElement.getBoundingClientRect().top + window.scrollY;
       if (
-        targetElement.offset().top <= scrollPosition &&
-        targetElement.offset().top + targetElement.height() > scrollPosition
+        targetPosition <= scrollPosition &&
+        targetPosition + targetElement.offsetHeight > scrollPosition
       ) {
-        // Mettez à jour la valeur du <select> pour correspondre à l'option actuellement visible
-        $("#mobileNavDropdown").val(targetId);
+        document.querySelector("#mobileNavDropdown").value = targetId;
       }
     }
   });
 });
 
-// ONE PAGE NAVIGATION
-
-// Fonction pour mettre à jour la classe "active" en fonction de la position de la page
 function updateActiveLink(menuHeight, navLinks) {
-  // Récupérez la position actuelle de la page
-  const scrollPosition = $(window).scrollTop() + 150;
+  const scrollPosition = window.scrollY + 150;
 
-  // Parcourez tous les liens de navigation
-  navLinks.each(function () {
-    const targetId = $(this).attr("href");
-    const targetElement = $(targetId);
+  navLinks.forEach(function (link) {
+    const targetId = link.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
 
-    // Vérifiez si la section est visible à l'écran
-    if (
-      targetElement.offset().top - menuHeight <= scrollPosition &&
-      targetElement.offset().top + targetElement.height() - menuHeight >
-        scrollPosition
-    ) {
-      // Ajoutez la classe "active" au lien correspondant
-      navLinks.removeClass("active");
-      $(this).addClass("active");
-    } else {
-      // Retirez la classe "active" des liens qui ne correspondent pas à la section visible
-      $(this).removeClass("active");
+    if (targetElement) {
+      const targetPosition =
+        targetElement.getBoundingClientRect().top + window.scrollY;
+      if (
+        targetPosition - menuHeight <= scrollPosition &&
+        targetPosition + targetElement.offsetHeight - menuHeight >
+          scrollPosition
+      ) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
     }
   });
 }
 
 function initializeOnePageNavigation() {
-  // Récupérez la hauteur du menu sticky
-  const menuHeight = $(".nav").outerHeight();
+  const menuHeight = document.querySelector(".nav").offsetHeight;
+  const navLinks = document.querySelectorAll(".smooth-scroll");
 
-  // Récupérez tous les liens de navigation avec la classe "smooth-scroll"
-  const navLinks = $(".smooth-scroll");
+  window.addEventListener("scroll", () =>
+    updateActiveLink(menuHeight, navLinks)
+  );
 
-  $(window).scroll(() => updateActiveLink(menuHeight, navLinks));
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      navLinks.forEach((link) => link.classList.remove("active"));
+      this.classList.add("active");
+      const targetId = this.getAttribute("href");
+      const targetPosition =
+        document.querySelector(targetId).getBoundingClientRect().top +
+        window.scrollY -
+        (menuHeight + 130);
 
-  // Lorsqu'un lien avec la classe "smooth-scroll" est cliqué
-  $(".smooth-scroll").on("click", function (event) {
-    event.preventDefault();
-
-    // Retirez la classe "active" de tous les liens
-    navLinks.removeClass("active");
-
-    // Ajoutez la classe "active" au lien cliqué
-    $(this).addClass("active");
-
-    // Récupérer l'ID de la section cible à partir de l'attribut href du lien
-    const targetId = $(this).attr("href");
-
-    // Animer le défilement vers la section cible tout en compensant la hauteur du menu
-    $("html, body").animate(
-      {
-        scrollTop: $(targetId).offset().top - (menuHeight + 130),
-      },
-      800 // Durée de l'animation en millisecondes (ajustez-la selon vos préférences)
-    );
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth",
+      });
+    });
   });
 }
 
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
   initializeOnePageNavigation();
 });
