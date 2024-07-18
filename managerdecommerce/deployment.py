82c5c852715c0d9d9c6fdb81a546e43261ea4229
Coco836 <coletteanselme@live.fr>
2024-07-18 12:23:01 +0200

    Update deployment.py

--- managerdecommerce/deployment.py
+++ managerdecommerce/deployment.py
@@ -3,7 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ["SECRET_KEY"]
-ALLOWED_HOSTS = ['169.254.130.3', os.environ['WEBSITE_HOSTNAME'], os.environ['CUSTOM_A_HOSTNAME'], os.environ['CUSTOM_CNAME_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], os.environ['CUSTOM_A_HOSTNAME'], os.environ['CUSTOM_CNAME_HOSTNAME'], os.environ['AZURE_INTERNAL_HEALTH_CHECK_IP']]
 CSRF_TRUSTED_ORIGINS = ["https://" + os.environ.get("WEBSITE_HOSTNAME"), "https://" + os.environ.get("CUSTOM_A_HOSTNAME"), "https://" + os.environ.get("CUSTOM_CNAME_HOSTNAME")]
 DEBUG = False
 
